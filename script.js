a476e52ae74918eb08a4c77c2cc2a6e1d920bd94
whydoifly <netpreecher@gmail.com>
2023-03-11 12:18:27 -0300

    Добавил массив

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ const sumForm = document.querySelector('.add-sum-form');
 const addSum = sumForm.querySelector('input');
 let budgetSum = 0;
 
+let categories = [
+  'Еда',
+  'Транспорт',
+  'Развлечения',
+  'Одежда',
+  'Красота',
+  'Здоровье',
+  'Другое',
+];
+
 sumForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(budgetSum = budgetSum + Number(addSum.value));
-    // document.getElementById('total-element').innerHTML = budgetSum;
-    document.getElementById('total-element').textContent = budgetSum;
-    sumForm.querySelector('input').value = "";
+  e.preventDefault();
+  console.log((budgetSum = budgetSum + Number(addSum.value)));
+  // document.getElementById('total-element').innerHTML = budgetSum;
+  document.getElementById('total-element').textContent = budgetSum;
+  sumForm.querySelector('input').value = '';
 });
+
+
